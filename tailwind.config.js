ccd8d4406228d190076a60a4b978bc23a941527d
gwynethtan <S10257317@connect.np.edu.sg>
2025-09-10 23:58:46 +0800

    build: added additional packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,69 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
 
+/** @type {import('tailwindcss').Config} **/
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js"
+  ],
+  theme: {
+	screens: {
+		'sm': '640px',   
+		'md': '768px',   
+		'lg': '1301px',   
+	  },
+  	extend: {
+  		colors: {
+  			transparent: 'transparent',
+  			card: 'rgba(217,217,217,0.1)',
+			formInputBox: 'rgba(217,217,217,0.1)',
+			purple:'#4B38AD',
+			turqoise:'#0CDFBF',
+			orange:'#DF800C',
+			yellow:'#DFC60C',
+			gray:'#B1B1B1',
+  			nav: '#06001e',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [flowbiteReact],
+};
